b0ff91c1142d8f7d845b669835499102b306716a
Nico Daßler <nico.dassler@fau.de>
2021-05-18 14:42:47 +0200

    FPSlider

---
+++ include/QTWC/FPSlider.h
@@ -0,0 +1,143 @@
+#pragma once
+
+#include <QSlider>
+
+namespace QTWC {
+
+	/**
+	 * Extension to QSlider to allow for floating point sliders.
+	 * 
+	 * Note: 
+	 *	Although some safety measures are implemented it might not be possible
+	 *  to use all the slider modification methods available in QSlider. setRange
+	 *  and setTickPosition should be safe to use. All others that influence the
+	 *	slider range and values might be unsafe.
+	 */
+	class FPSlider: public QSlider {
+
+		Q_OBJECT
+
+	public:
+
+		/**
+		 * Create floating point slider.
+		 *
+		 * Same as constructor from QSlider with same signature.
+		 *
+		 * This initializes the slider to a 0.0 - 1.0 range with step size 0.01.
+		 *
+		 * @param orientation horizontal or vertical slider.
+		 * @param parent pointer to the parent widget.
+		 */
+		FPSlider(Qt::Orientation orientation, QWidget* parent = nullptr);
+
+		/**
+		 * Create floating point slider.
+		 *
+		 * Same as constructor from QSlider with same signature.
+		 *
+		 * This initializes the slider to a 0.0 - 1.0 range with step size 0.01.
+		 *
+		 * @param parent pointer to the parent widget.
+		 */
+		FPSlider(QWidget* parent = nullptr);
+
+		/**
+		 * Create floating point slider.
+		 *
+		 * This initializes the slider according to the given parameters.
+		 * 
+		 * Usage example: 
+		 *	- Want: Slider from 5 to 10 with step size 0.25
+		 *  - Set: rangeBegin to 5, rangeEnd to 10 and stepSize to 0.25.
+		 *
+		 * @param orientation horizontal or vertical slider.
+		 * @param rangeBegin: The lowest value of the slider (must be smaller than rangeEnd).
+		 * @param rangeEnd: The largest value of the slider.
+		 * @param stepSize: The size of one step of the slider (must be smaller than or equal to rangeEnd-rangeBegin).
+		 * @param parent pointer to the parent widget.
+		 */
+		FPSlider(Qt::Orientation orientation, int rangeBegin, int rangeEnd, 
+			float stepSize, QWidget* parent = nullptr);
+
+		virtual ~FPSlider() = default;
+
+		FPSlider(const FPSlider&) = default;
+
+		FPSlider(FPSlider&&) = default;
+
+		FPSlider& operator=(const FPSlider&) = default;
+
+		FPSlider& operator=(FPSlider&&) = default;
+
+	signals:
+
+		/**
+		 * Same as valueChanged(int) of qslider but with floating point value.
+		 *
+		 * @param value the value that the slider changed to.
+		 */
+		void fpValueChanged(float value);
+
+	private slots:
+
+		/**
+		 * Slot for receiving the changed value of the underlying normal slider.
+		 *
+		 * @param value the value of the slider
+		 */
+		void receiveValueChanged(int value);
+
+		/**
+		 * Slot for receiving changes in the range of the slider.
+		 *
+		 * This is mostly a safety measure in case someone decides to change the sliders range
+		 * afterwards.
+		 *
+		 * @param min minimum value of the slider.
+		 * @param max maximum value of the slider.
+		 */
+		void receiveRangeChanged(int min, int max);
+
+
+	private:
+
+		/**
+		 * Helper for initializing.
+		 *
+		 * Default initialization:
+		 * --- range: 0-100
+		 * --- step size: 1
+		 * --- ticks: both sides:
+		 * --- divisor: 100
+		 */
+		void initializeFPSlider();
+
+		/**
+		 * Helper for initializing.
+		 *
+		 * Manual initialization.
+		 *
+		 * @param rangeEnd the upper end of the slider.
+		 */
+		void initializeFPSlider(int rangeEnd);
+
+		/**
+		 * Helper to wrap various calls to connect().
+		 */
+		void setupConnections();
+
+
+		/**
+		 * The step size.
+		 */
+		float mStepSize;
+
+		/**
+		 * The lowest value of the slider.
+		 */
+		int mRangeBegin;
+
+	};
+}
+
---
+++ source/FPSlider.cpp
@@ -0,0 +1,81 @@
+#include "QTWC/FPSlider.h"
+
+#include <exception>
+
+#include <QSignalBlocker>
+
+namespace QTWC {
+
+	FPSlider::FPSlider(Qt::Orientation orientation, QWidget* parent) 
+		: QSlider(orientation, parent)
+		, mStepSize(1.0)
+	{
+		initializeFPSlider();
+		setupConnections();
+	}
+
+	FPSlider::FPSlider(QWidget* parent)
+		: QSlider(parent)
+		, mStepSize(1.0)
+	{
+		initializeFPSlider();
+		setupConnections();
+	}
+
+	FPSlider::FPSlider(Qt::Orientation orientation, int rangeBegin, int rangeEnd,
+		float stepSize, QWidget* parent)
+		: QSlider(orientation, parent)
+		, mStepSize(stepSize)
+		, mRangeBegin(rangeBegin)
+	{
+		initializeFPSlider(rangeEnd);
+		setupConnections();
+	}
+
+	void FPSlider::initializeFPSlider() {
+		this->setRange(0, 100);
+		this->setTickInterval(1);
+		this->setTickPosition(QSlider::TickPosition::TicksBothSides);
+		mStepSize = 0.01;
+	}
+
+	void FPSlider::initializeFPSlider(int rangeEnd) {
+		if (mRangeBegin >= rangeEnd) {
+			throw std::invalid_argument("rangeBegin must be smaller than rangeEnd!");
+		}
+		if (mStepSize > (rangeEnd - mRangeBegin)) {
+			throw std::invalid_argument("Step size must be smaller or equal to interval length!");
+		}
+		// compute length of underlying slider
+		int length = static_cast<int>((rangeEnd - mRangeBegin) / mStepSize);
+		// always start from 0
+		this->setRange(0, length);
+		this->setTickInterval(1);
+		this->setTickPosition(QSlider::TickPosition::TicksBothSides);
+	}
+
+	void FPSlider::setupConnections() {
+		// connect valueChanged of underlying slider with receiveValueChanged to make it a float value
+		connect(this, &QSlider::valueChanged, this, &FPSlider::receiveValueChanged);
+		// connect rangeChanged of underlying slider with receiveRangeChanged to adapt FPSlider
+		connect(this, &QSlider::rangeChanged, this, &FPSlider::receiveRangeChanged);
+	}
+
+	void FPSlider::receiveValueChanged(int value) {
+		// recalculate and emit
+		float floatValue = mRangeBegin + (value * mStepSize);
+		emit fpValueChanged(floatValue);
+	}
+
+	void FPSlider::receiveRangeChanged(int min, int max) {
+		// we will adapt the range in the same way as the slider was initialized
+		{
+			const QSignalBlocker blocker(this);
+			mRangeBegin = min;
+			initializeFPSlider(max);
+		}
+	}
+
+}
+
+
